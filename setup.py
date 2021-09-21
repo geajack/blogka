10c5820e73fafcfe127284f3c2c578a9adbbd674
Jack <->
2021-09-21 22:29:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/geajack/blogka",
-    packages=["blogka"],
+    py_modules=["blogka"],
     include_package_data=True,
     classifiers=[
     ],
